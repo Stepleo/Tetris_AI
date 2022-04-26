ee3145f0da0f86409580595bc24bf389aa22d1e3
Léo Stepien <stepleo66@gmail.com>
2022-04-26 15:43:27 +0200

    Entrainement pour les blocs 1 à 1, il faut encore générer une grille aléatoire avant de faire tourner

--- AI.py
+++ AI.py
@@ -29,7 +29,7 @@ def run(config_file):
     p.add_reporter(neat.Checkpointer(5))
 
     # Run for up to 100 generations.
-    winner = p.run(eval_genomes, 100)
+    winner = p.run(evaluation_genome, 10)
 
     # Display the winning genome.
     # print('\nBest genome:\n{!s}'.format(winner))
@@ -37,24 +37,19 @@ def run(config_file):
     node_names = {-2:'Gauche',
                   -1: 'Droite',
                   0:'Rotation',
-                  1 :'Position', #tetrimino.x, terrimino.y, tetrimino.rotation(=pos dans liste)
-                  2:'Espace_libre'}
+                  1 :'Position_x',
+                  2:'Postion_y',
+                  3:'Position_alpha'}
     visualize.draw_net(config, winner, True, node_names=node_names)
     visualize.plot_stats(stats, ylog=False, view=True)
     visualize.plot_species(stats, view=True)
 
     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
-    p.run(fitnss, 10)
+    p.run(evaluation_genome, 10)
 
 
 
-
-
-
-
-
-
-def fitness(config, genomes):
+def evaluation_genome(genomes, config):
     positions_statiques = {}
     grille = creation_grille(positions_statiques)
 
@@ -68,68 +63,80 @@ def fitness(config, genomes):
     vitesse_chute = 0.33
     score = 0
 
-    while run:
-        grille = creation_grille(positions_statiques)
-        temps_de_chute += temps.get_rawtime()
-        temps_de_jeu += temps.get_rawtime()
-        temps.tick()
 
-        if temps_de_chute / 1000 > vitesse_chute:
-            temps_de_chute = 0
-            tetrimino_actuel.y += 1
-            if not (espace_disponible(tetrimino_actuel, grille)) and tetrimino_actuel.y > 0:
-                tetrimino_actuel.y -= 1
-                changement_tetrimino = True
+    nets = []
+    tetrimino_training = []
+    g = []
 
-        if temps_de_jeu / 1000 > 5:
-            temps_de_jeu = 0
-            if vitesse_chute > 0.12:
-                vitesse_chute -= 0.005
+    for genome_id, genome in genomes:
+        genome.fitness = 0  # start with fitness level of 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        tetrimino_training.append(obtenir_forme())
+        g.append(genome)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+
+
+    for i,t in enumerate(tetrimino_training):
+        output = nets[i].activate(t.x, t.y, t.rotation )
+        while run:
+            temps_de_chute += temps.get_rawtime()
+            temps_de_jeu += temps.get_rawtime()
+            temps.tick()
+
+            if temps_de_chute / 1000 > vitesse_chute:
+                temps_de_chute = 0
+                t.y += 1
+                if not (espace_disponible(t, grille)) and t.y > 0:
+                    t.y -= 1
+                    changement_tetrimino = True
+
+
+            if temps_de_jeu / 1000 > 5:
+                temps_de_jeu = 0
+                if vitesse_chute > 0.12:
+                    vitesse_chute -= 0.005
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+                if output[i][0] > 0.5:
+                    t.x -= 1
+                    if not (espace_disponible(t, grille)):
+                        t.x += 1
+                if output[i][1] > 0.5:
+                    t.x += 1
+                    if not (espace_disponible(t, grille)):
+                        t.x -= 1
+                if output[i][2] > 0.5:
+                    t.rotation += 1
+                    if not (espace_disponible(t, grille)):
+                        t.rotation -= 1
+
+
+
+            tetrimino_pos = conversion_format(t)
+
+            for j in range(len(tetrimino_pos)):
+                x, y = tetrimino_pos[j]
+                if y > -1:
+                    grille[y][x] = t.couleur
+
+            if changement_tetrimino:
+                for pos in tetrimino_pos:
+                    p = (pos[0], pos[1])
+                    positions_statiques[p] = t.couleur
+                changement_tetrimino = False
+                g[i] += clear_rows(grille, positions_statiques) * 5
                 run = False
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    tetrimino_actuel.x -= 1
-                    if not (espace_disponible(tetrimino_actuel, grille)):
-                        tetrimino_actuel.x += 1
-                if event.key == pygame.K_RIGHT:
-                    tetrimino_actuel.x += 1
-                    if not (espace_disponible(tetrimino_actuel, grille)):
-                        tetrimino_actuel.x -= 1
-                if event.key == pygame.K_DOWN:
-                    tetrimino_actuel.y += 1
-                    if not (espace_disponible(tetrimino_actuel, grille)):
-                        tetrimino_actuel.y -= 1
-                if event.key == pygame.K_UP:
-                    tetrimino_actuel.rotation += 1
-                    if not (espace_disponible(tetrimino_actuel, grille)):
-                        tetrimino_actuel.rotation -= 1
-
-        tetrimino_pos = conversion_format(tetrimino_actuel)
-
-        for i in range(len(tetrimino_pos)):
-            x, y = tetrimino_pos[i]
-            if y > -1:
-                grille[y][x] = tetrimino_actuel.couleur
-
-        if changement_tetrimino:
-            for pos in tetrimino_pos:
-                p = (pos[0], pos[1])
-                positions_statiques[p] = tetrimino_actuel.couleur
-            tetrimino_actuel = prochain_tetrimino
-            prochain_tetrimino = obtenir_forme()
-            changement_tetrimino = False
-            score += clear_rows(grille, positions_statiques) * 10
 
         dessine_fenetre(win, grille, score)
         dessine_prochaine_forme(prochain_tetrimino, win)
         pygame.display.update()
 
-        if check_lost(positions_statiques):
-            dessine_texte_milieu("PERDU", 80, (255, 255, 255), win)
-            pygame.display.update()
-            pygame.time.delay(1500)
-            run = False
+        #if check_lost(positions_statiques):
+        #   dessine_texte_milieu("PERDU", 80, (255, 255, 255), win)
+        #    pygame.display.update()
+        #    pygame.time.delay(1500)
+        #    run = False
